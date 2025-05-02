1da33d2d0e11be8dc86661feaf852db90c073f58
jocooper <jocooper2018@gmail.com>
2025-05-02 09:15:25 +0200

    feat: Now a message tell us when I put an incorrect email or password on the login page.

--- frontend/src/Pages/Login/index.tsx
+++ frontend/src/Pages/Login/index.tsx
@@ -5,6 +5,7 @@ import { emailRegex } from '../../utils/regex';
 import Input from '../../Components/Input';
 import { User } from '../..';
 import { useLogInMutation } from '../../graphql';
+import { ApolloError } from '@apollo/client';
 
 interface LoginProps {
   loggedUser: User | undefined;
@@ -12,68 +13,80 @@ interface LoginProps {
 }
 
 export default function Login(props: LoginProps) {
-
-  const [emailInputValue, setEmailInputValue] = useState('');
-  const [passwordInputValue, setPasswordInputValue] = useState('');
+  const [emailInputValue, setEmailInputValue] = useState<string>('');
+  const [passwordInputValue, setPasswordInputValue] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const [fetchLogIn] = useLogInMutation();
 
   const login: () => Promise<void> = async (): Promise<void> => {
-    const queryResult = await fetchLogIn({
-      variables: {
-        logInDto: {
-          email: emailInputValue,
-          password: passwordInputValue,
+    setErrorMessage('');
+    try {
+      const queryResult = await fetchLogIn({
+        variables: {
+          logInDto: {
+            email: emailInputValue,
+            password: passwordInputValue,
+          },
         },
-      },
-    });
-    if (!queryResult.data?.logIn) {
-      return ;
+      });
+      if (!queryResult.data?.logIn) {
+        return;
+      }
+      const loggedUser: User = queryResult.data?.logIn;
+      props.setLoggedUser(loggedUser);
+    } catch (error) {
+      if (error instanceof ApolloError) {
+        if (error.message === 'Incorrect login or password') {
+          setErrorMessage('Email ou mot de passe incorrect');
+          return;
+        }
+      }
+      console.error(error);
     }
-    const loggedUser: User = queryResult.data?.logIn;
-    props.setLoggedUser(loggedUser);
   };
 
-  return props.loggedUser ?
-    <Navigate to="/" /> :
-    (
-      <main className="login-page">
-        <h1>Connexion</h1>
-        <span>
+  return props.loggedUser ? (
+    <Navigate to="/" />
+  ) : (
+    <main className="login-page">
+      <h1>Connexion</h1>
+      <span>
         Vous n'avez pas encore de compte&nbsp;?{' '}
-          <Link to={'/signup'}>Créez-en un&nbsp;!</Link>
+        <Link to={'/signup'}>Créez-en un&nbsp;!</Link>
       </span>
-        <Input
-          type="email"
-          id="login-email-input"
-          placeholder="Email"
-          state={{
-            inputValue: emailInputValue,
-            setInputValue: setEmailInputValue,
-          }}
-        >
-          Email
-        </Input>
-        <Input
-          type="password"
-          id="login-password-input"
-          placeholder="Mot de passe"
-          state={{
-            inputValue: passwordInputValue,
-            setInputValue: setPasswordInputValue,
-          }}
-        >
-          Mot de passe
-        </Input>
-        <button
-          type="button"
-          onClick={login}
-          disabled={
-            !emailRegex.test(emailInputValue) || passwordInputValue.length < 8
-          }
-        >
-          Se connecter
-        </button>
-      </main>
-    );
+      <Input
+        type="email"
+        id="login-email-input"
+        placeholder="Email"
+        state={{
+          inputValue: emailInputValue,
+          setInputValue: setEmailInputValue,
+        }}
+      >
+        Email
+      </Input>
+      <Input
+        type="password"
+        id="login-password-input"
+        placeholder="Mot de passe"
+        state={{
+          inputValue: passwordInputValue,
+          setInputValue: setPasswordInputValue,
+        }}
+      >
+        Mot de passe
+      </Input>
+      <button
+        type="button"
+        onClick={login}
+        disabled={
+          !emailRegex.test(emailInputValue) || passwordInputValue.length < 8
+        }
+      >
+        Se connecter
+      </button>
+      <div className="error-message">{errorMessage}</div>
+    </main>
+  );
 }
