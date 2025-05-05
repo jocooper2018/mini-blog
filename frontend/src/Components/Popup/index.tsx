81b2f11ecee49e3b51d9ec4c7d43d09a6823cd01
jocooper <jocooper2018@gmail.com>
2025-05-05 09:27:34 +0200

    feat: Added popup component

---
+++ frontend/src/Components/Popup/index.tsx
@@ -0,0 +1,50 @@
+import './index.css';
+import { ReactNode, useState } from 'react';
+
+interface PopupProps {
+  actionText: string;
+  class?: string;
+  action?: () => void;
+  children: ReactNode;
+}
+
+export default function Popup(props: PopupProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return isOpen ? (
+    <div className="popup">
+      <div>
+        <div>{props.children}</div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
+            {props.action ? 'Annuler' : 'Fermer'}
+          </button>
+          {props.action ? (
+            <button
+              type="button"
+              className={props.class}
+              onClick={props.action}
+            >
+              {props.actionText}
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <button
+      type="button"
+      className={props.class}
+      onClick={() => {
+        setIsOpen(true);
+      }}
+    >
+      {props.actionText}
+    </button>
+  );
+}
